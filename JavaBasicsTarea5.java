923395d7ebe811ff60a0140c6e31aafe9abe86d0
FGomis <fkgomis@gmail.com>
2021-07-07 11:44:56 +0200

    Update JavaBasicsTarea5.java

--- JavaBasicsTarea5.java
+++ JavaBasicsTarea5.java
@@ -9,11 +9,20 @@ public class JavaBasicsTarea5 {
 		int D = 4;
 		
 		B=C;
+		
+		System.out.println("B es "+B+" y C es "+C);
+		
 		C=A;
+		
+		System.out.println("C es "+C+" y A es "+A);
+		
 		A=D;
+		
+		System.out.println("A es "+A+" y D es "+D);
+		
 		D=B;
 		
-		System.out.println(A+" "+B+" "+C+" "+D);
+		System.out.println("D es "+D+" y B es "+B);
 
 	}
 
