7aab7d62a2628e719ce31e4e1467957c416a6def
FGomis <fkgomis@gmail.com>
2021-07-07 11:17:41 +0200

    Create JavaBasicsTarea5.java

---
+++ JavaBasicsTarea5.java
@@ -0,0 +1,20 @@
+
+public class JavaBasicsTarea5 {
+
+	public static void main(String[] args) {
+		
+		int A = 1;
+		int B = 2;
+		int C = 3;
+		int D = 4;
+		
+		B=C;
+		C=A;
+		A=D;
+		D=B;
+		
+		System.out.println(A+" "+B+" "+C+" "+D);
+
+	}
+
+}
